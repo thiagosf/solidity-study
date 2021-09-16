7f9e0e0796c29defddc8afe048f32e46bbb346da
Thiago Silva Ferreira <thiagosfsjc@gmail.com>
2021-09-16 17:48:36 -0300

    WIP modules

---
+++ README.md
@@ -0,0 +1,17 @@
+# Solidity Inbox Study
+
+Study of Udemy Solidity Course:
+
+[https://www.udemy.com/course/ethereum-and-solidity-the-complete-developers-guide](https://www.udemy.com/course/ethereum-and-solidity-the-complete-developers-guide)
+
+## Install dependencies
+
+```bash
+npm ci
+```
+
+## Test
+
+```bash
+npm test
+```
---
+++ test/Inbox.test.js
@@ -0,0 +1,30 @@
+const assert = require('assert')
+const ganache = require('ganache-cli')
+const Web3 = require('web3')
+const web3 = new Web3(ganache.provider())
+const { interface, bytecode } = require('../compile')
+
+describe('Inbox', () => {
+  let accounts
+  let inbox
+
+  beforeEach(async () => {
+    // Get a list of all accounts
+    accounts = await web3.eth.getAccounts()
+
+    // Use one of those accounts to deploy
+    // the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+      .deploy({ data: bytecode, arguments: ['Initial message'] })
+      .send({ from: accounts[0], gas: '1000000' })
+  })
+
+  it('deploys a contract', async () => {
+    assert.ok(inbox.options.address)
+  })
+
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call()
+    assert.strictEqual(message, 'Initial message')
+  })
+})
