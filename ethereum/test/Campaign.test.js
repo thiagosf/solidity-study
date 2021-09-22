0591b7faafcccd1a06c5bab138275e6b50f8e971
Thiago Silva Ferreira <thiagosfsjc@gmail.com>
2021-09-22 14:49:51 -0300

    Campaign tests done

--- ethereum/test/Campaign.test.js
+++ ethereum/test/Campaign.test.js
@@ -46,4 +46,73 @@ describe('Campaign Contract', function () {
     assert.ok(factory.options.address)
     assert.ok(campaign.options.address)
   })
+
+  it('marks caller as the campaign manager', async () => {
+    const manager = await campaign.methods.manager().call()
+    assert.strictEqual(manager, accounts[0])
+  })
+
+  it('allows people to contribute money and marks them as approvers', async () => {
+    await campaign.methods.contribute().send({
+      from: accounts[1],
+      value: '200',
+    })
+    const isContributer = await campaign.methods.approvers(accounts[1]).call()
+    assert(isContributer)
+  })
+
+  it('requires a minimum contribution', async () => {
+    await assert.rejects(
+      campaign.methods.contribute().send({
+        from: accounts[1],
+        value: '5'
+      })
+    )
+  })
+
+  it('allows a manager to make a payment request', async () => {
+    await campaign.methods.createRequest(
+      'Buy batteries',
+      '100',
+      accounts[1]
+    ).send({
+      from: accounts[0],
+      gas: '1000000'
+    })
+    const request = await campaign.methods.requests(0)
+      .call()
+    assert.strictEqual(request.description, 'Buy batteries')
+  })
+
+  it('proccess requests', async () => {
+    await campaign.methods.contribute().send({
+      from: accounts[0],
+      value: web3.utils.toWei('10', 'ether'),
+    })
+
+    await campaign.methods.createRequest(
+      'Buy batteries',
+      web3.utils.toWei('5', 'ether'),
+      accounts[1]
+    ).send({
+      from: accounts[0],
+      gas: '1000000',
+    })
+
+    await campaign.methods.approveRequest(0)
+      .send({
+        from: accounts[0],
+        gas: '1000000',
+      })
+
+    await campaign.methods.finalizeRequest(0)
+      .send({
+        from: accounts[0],
+        gas: '1000000',
+      })
+
+    let balance = await web3.eth.getBalance(accounts[1])
+    balance = parseFloat(web3.utils.fromWei(balance, 'ether'))
+    assert(balance > 104)
+  })
 })
